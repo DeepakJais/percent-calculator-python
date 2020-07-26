45ecccc4492b3ee1c71ca57937072a7a5db3cfad
Deepak Jaiswar <DeepakJaiswar678@gmail.com>
2020-07-26 11:23:37 +0530

    Update percentage calculator.py

--- percentage calculator.py
+++ percentage calculator.py
@@ -1,11 +1,23 @@
+# Created By 
+# Deepak Jaiswar 25/7/20
+
 print("Percentage Calculator ")
 print("")
+
+#Input Your Marks
 x = float(input("Your Marks = "))
+
+#per multiplying number
 per = 100
+
+#Input Sub Total Marks
 y = float(input("Total Marks = "))
 print("")
+
+#Here we put aur formula
 z = x * per / y
 print("")
+
 print("You Got = %.2f " %z)
 
 if z >= 35:
